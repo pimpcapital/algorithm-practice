103172be3be2259af444107818312531ff0a9c44
beijunyi <beijunyi@gmail.com>
2015-01-14 18:12:41 +0000

    bit manipulations

---
+++ src/com/beijunyi/leetcode/SingleNumberTwo.java
@@ -0,0 +1,67 @@
+package com.beijunyi.leetcode;
+
+/**
+ * Given an array of integers, every element appears three times except for one. Find that single one.
+ *
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+public class SingleNumberTwo {
+
+  /**
+   * Consider every bit in a number, if this bit appear 3 times then set it to zero.
+   * so here is 3 state of one bit, we can use two bit to indicate it.
+   * we use 00 -> 01 -> 10 -> 00...
+   *
+   * 00: bit 1 appear 0 time.
+   * 01: bit 1 appear 1 time.
+   * 10: bit 1 appear 2 times.
+   *
+   * so every bit 1 appear 3 times, the state will go to 00
+   * but for the single number, every bit 1 in it, its status is 01
+   * we can use two integer to represent the status, named first, second
+   *
+   * Now problem is how to generate the code to change 'first' and 'second'
+   * I use truth table to generate the code, like this:
+   * count every '1' bit, 3 times to zero
+   * so we need 3 status, 2 bit is enough
+   *
+   * state: 00 -> 01 -> 10 -> 00....
+   *
+   * so we can draw a truth table of first bit
+   *   A: first bit
+   *   B: second bit
+   *   C: input bit
+   *
+   *   A  B  C NA NB
+   *   0  0  0  0  0
+   *   0  0  1  0  1
+   *   0  1  0  0  1
+   *   0  1  1  1  0
+   *   1  0  0  1  0
+   *   1  0  1  0  0
+   *   1  1  0  \  \
+   *   1  1  1  \  \
+   *
+   * so
+   *   NA = ~ABC + A~B~C
+   *   NB = ~A~BC + ~AB~C = ~A(B^C)
+   *   NB is first, NA is second, so we should use last first when we calc next second
+   */
+  public static class Solution {
+    public int singleNumber(int[] A) {
+      int ones = 0, twos = 0;
+      for(int a : A) {
+        ones = (ones ^ a) & ~twos;
+        twos = (twos ^ a) & ~ones;
+      }
+      return ones;
+    }
+  }
+
+  public static void main(String args[]) {
+    System.out.println(new Solution().singleNumber(new int[]{3, 2, 1, 2, 2, 3, 3}));
+  }
+
+
+}
---
+++ src/com/beijunyi/leetcode/Subsets.java
@@ -0,0 +1,102 @@
+package com.beijunyi.leetcode;
+
+import java.util.*;
+
+/**
+ * Given a set of distinct integers, S, return all possible subsets.
+ *
+ * Note:
+ * Elements in a subset must be in non-descending order.
+ * The solution set must not contain duplicate subsets.
+ * For example,
+ * If S = [1,2,3], a solution is:
+ *
+ *   [
+ *     [3],
+ *     [1],
+ *     [2],
+ *     [1,2,3],
+ *     [1,3],
+ *     [2,3],
+ *     [1,2],
+ *     []
+ *   ]
+ */
+public class Subsets {
+
+  public static class Solution1 {
+    public List<List<Integer>> subsets(int[] S) {
+      List<List<Integer>> results = new ArrayList<>();
+      results.add(new ArrayList<Integer>());
+      Arrays.sort(S);
+      for(int curr : S) {
+        List<List<Integer>> cache = new ArrayList<>();
+        for(List<Integer> lst : results) {
+          List<Integer> copy = new ArrayList<>(lst);
+          copy.add(curr);
+          cache.add(copy);
+        }
+        results.addAll(cache);
+      }
+      return results;
+    }
+  }
+
+  /**
+   * Number of subsets for {1 , 2 , 3 } = 2^3 .
+   * why ?
+   * case    possible outcomes for the set of subsets
+   * 1   ->          Take or dont take = 2
+   * 2   ->          Take or dont take = 2
+   * 3   ->          Take or dont take = 2
+   *
+   * therefore , total = 2*2*2 = 2^3 = { { } , {1} , {2} , {3} , {1,2} , {1,3} , {2,3} , {1,2,3} }
+   *
+   * Lets assign bits to each outcome  -> First bit to 1 , Second bit to 2 and third bit to 3
+   * Take = 1
+   * Dont take = 0
+   *
+   * 0) 0 0 0  -> Dont take 3, Dont take 2, Dont take 1 = { }
+   * 1) 0 0 1  -> Dont take 3, Dont take 2,      take 1 = { 1 }
+   * 2) 0 1 0  -> Dont take 3,      take 2, Dont take 1 = { 2 }
+   * 3) 0 1 1  -> Dont take 3,      take 2,      take 1 = { 1, 2 }
+   * 4) 1 0 0  ->      take 3, Dont take 2, Dont take 1 = { 3 }
+   * 5) 1 0 1  ->      take 3, Dont take 2,      take 1 = { 1, 3 }
+   * 6) 1 1 0  ->      take 3,      take 2, Dont take 1 = { 2, 3 }
+   * 7) 1 1 1  ->      take 3,      take 2,      take 1 = { 1, 2 , 3 }
+   *
+   * In the above logic ,Insert S[i] only if (j>>i)&1 ==true   { j E { 0,1,2,3,4,5,6,7 }   i = ith element in the input array }
+   *
+   * element 1 is inserted only into those places where 1st bit of j is 1
+   * if( j >> 0 &1 )  ==> for above above eg. this is true for sl.no.( j )= 1 , 3 , 5 , 7
+   *
+   * element 2 is inserted only into those places where 2nd bit of j is 1
+   * if( j >> 1 &1 )  == for above above eg. this is true for sl.no.( j ) = 2 , 3 , 6 , 7
+   *
+   * element 3 is inserted only into those places where 3rd bit of j is 1
+   * if( j >> 2 & 1 )  == for above above eg. this is true for sl.no.( j ) = 4 , 5 , 6 , 7
+   *
+   * Time complexity : O(n*2^n) , for every input element loop traverses the whole solution set length i.e. 2^n
+   */
+  public static class Solution2 {
+    public List<List<Integer>> subsets(int[] S) {
+      Arrays.sort(S);
+      int totalNumber = 1 << S.length;
+      List<List<Integer>> collection = new ArrayList<>(totalNumber);
+      for(int i = 0; i < totalNumber; i++) {
+        List<Integer> set = new LinkedList<>();
+        for(int j = 0; j < S.length; j++)
+          if((i & (1 << j)) != 0)
+            set.add(S[j]);
+        collection.add(set);
+      }
+      return collection;
+    }
+  }
+
+  public static void main(String args[]) {
+    System.out.println(new Solution1().subsets(new int[]{1, 2, 3}));
+    System.out.println(new Solution2().subsets(new int[]{1, 2, 3}));
+  }
+
+}

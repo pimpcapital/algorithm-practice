177bd99660557a918bb48bedf2e8b92501e042f7
beijunyi <junyi.bei@formicary.net>
2015-01-19 17:02:15 +0000

    median of two sorted arrays

--- src/com/beijunyi/leetcode/MedianOfTwoSortedArrays.java
+++ src/com/beijunyi/leetcode/MedianOfTwoSortedArrays.java
@@ -1,7 +1,26 @@
 package com.beijunyi.leetcode;
 
+/**
+ * There are two sorted arrays A and B of size m and n respectively.
+ * Find the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n)).
+ */
 public class MedianOfTwoSortedArrays {
 
+  /**
+   * This is my iterative solution using binary search.
+   * The main idea is to find the approximate location of the median and compare the elements around it to get the final result.
+   *
+   * do binary search. suppose the shorter list is A with length n.
+   * the runtime is O(log(n)) which means no matter how large B array is, it only depends on the size of A.
+   * It makes sense because if A has only one element while B has 100 elements, the median must be one of A[0], B[49], and B[50] without check everything else.
+   * If A[0] <= B[49], B[49] is the answer; if B[49] < A[0] <= B[50], A[0] is the answer; else, B[50] is the answer.
+   *
+   * After binary search, we get the approximate location of median.
+   * Now we just need to compare at most 4 elements to find the answer. This step is O(1).
+   *
+   * the same solution can be applied to find kth element of 2 sorted arrays.
+   */
   public static class Solution {
 
     public double findMedianSortedArrays(int A[], int B[]) {

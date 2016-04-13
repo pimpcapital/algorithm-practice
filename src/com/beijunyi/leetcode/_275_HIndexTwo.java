3706695229b918fd6ae47d2643d350f38ae8a4e6
Junyi Bei <beijunyi@gmail.com>
2016-04-13 11:58:48 +0100

    _275_HIndexTwo

--- src/com/beijunyi/leetcode/_275_HIndexTwo.java
+++ src/com/beijunyi/leetcode/_275_HIndexTwo.java
@@ -1,5 +1,10 @@
 package com.beijunyi.leetcode;
 
+import java.util.Arrays;
+
+import com.beijunyi.leetcode.category.solution.BinarySearch;
+import com.beijunyi.leetcode.category.solution.Recursive;
+
 /**
  * Follow up for H-Index: What if the citations array is sorted in ascending order? Could you optimize your algorithm?
  *
@@ -12,11 +17,44 @@ public class _275_HIndexTwo {
     int hIndex(int[] citations);
   }
 
-  public static class Solution1 implements Solution {
+  /**
+   * Time: O(log(n)), Space: O(log(n))
+   */
+  public static class Solution1 implements Solution, BinarySearch, Recursive {
 
     @Override
     public int hIndex(int[] citations) {
-      return 0;
+      return hIndex(citations, 0, citations.length);
+    }
+
+    private static int hIndex(int[] citations, int start, int end) {
+      int mid = (start + end) / 2;
+      if(mid >= citations.length) return 0;
+      int c = citations[mid];
+      int count = citations.length - mid; // number of values >= c
+      if(mid == start) return Math.min(count, c); // remain search space is 0
+      return count <= c
+               ? Math.max(count, hIndex(citations, start, mid))
+               : hIndex(citations, mid + 1, end);
+    }
+  }
+
+  /**
+   * Time: O(log(n)), Space: O(1)
+   */
+  public static class Solution2 implements Solution, BinarySearch {
+
+    public int hIndex(int[] citations) {
+      if(citations == null || citations.length == 0) return 0;
+      int l = 0, r = citations.length;
+      int n = citations.length;
+      while(l < r){
+        int mid = l + (r - l) / 2;
+        if(citations[mid] == n - mid) return n - mid;
+        if(citations[mid] < citations.length - mid) l = mid + 1;
+        else r = mid;
+      }
+      return n - l;
     }
 
   }
@@ -25,17 +63,32 @@ public class _275_HIndexTwo {
     int[] citations;
     int r;
 
-    Solution s1 = new Solution1();
-
-    citations = new int[] {0, 1, 3, 5, 6};
-    r = s1.hIndex(citations);
-    System.out.println(r);
-    citations = new int[] {0};
-    r = s1.hIndex(citations);
-    System.out.println(r);
-    citations = new int[] {1};
-    r = s1.hIndex(citations);
-    System.out.println(r);
+    for(Solution s : Arrays.asList(new Solution1(), new Solution2())) {
+      citations = new int[] {0, 1, 3, 5, 6};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {0, 0, 4, 4};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {0, 0};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {0, 1};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {1, 2};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {11, 15};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {0};
+      r = s.hIndex(citations);
+      System.out.println(r);
+      citations = new int[] {1};
+      r = s.hIndex(citations);
+      System.out.println(r);
+    }
   }
 
 }

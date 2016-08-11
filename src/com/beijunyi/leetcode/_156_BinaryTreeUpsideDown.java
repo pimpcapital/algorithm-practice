ca8c0fd4fb09bdfdeee2d8b24b238d7a153e3934
beijunyi <beijunyi@gmail.com>
2016-08-11 01:19:24 +0100

    _156_BinaryTreeUpsideDown

---
+++ src/com/beijunyi/leetcode/_156_BinaryTreeUpsideDown.java
@@ -0,0 +1,64 @@
+package com.beijunyi.leetcode;
+
+import java.util.Arrays;
+
+import com.beijunyi.leetcode.category.PremiumQuestion;
+import com.beijunyi.leetcode.category.difficulty.Medium;
+import com.beijunyi.leetcode.ds.TreeNode;
+
+/**
+ * Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same
+ * parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left
+ * leaf nodes. Return the new root.
+ *
+ * For example:
+ *   Given a binary tree {1,2,3,4,5},
+ *       1
+ *      / \
+ *     2   3
+ *    / \
+ *   4   5
+ * return the root of the binary tree [4,5,2,#,#,3,1].
+ *       4
+ *      / \
+ *     5   2
+ *        / \
+ *       3   1
+ */
+public class _156_BinaryTreeUpsideDown implements Medium, PremiumQuestion {
+
+  public interface Solution {
+    TreeNode upsideDownBinaryTree(TreeNode root);
+  }
+
+  public static class Solution1 implements Solution {
+    @Override
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+      if(root == null) return null;
+      if(root.left == null && root.right == null) return root;
+
+      TreeNode newRoot = root.left;
+      TreeNode newLeft = root.right;
+      TreeNode newRight = root;
+      newRight.left = null;
+      newRight.right = null;
+
+      TreeNode ret = upsideDownBinaryTree(newRoot);
+      assert newRoot != null;
+      newRoot.left = newLeft;
+      newRoot.right = newRight;
+      return ret;
+    }
+  }
+
+  public static void main(String args[]) {
+    TreeNode root;
+    TreeNode result;
+
+    for(Solution s : Arrays.asList(new Solution1())) {
+      root = TreeNode.fromArray(1, 2, 3, 4, 5);
+      result = s.upsideDownBinaryTree(root);
+      System.out.println(Arrays.toString(TreeNode.toArray(result)));
+    }
+  }
+}
--- src/com/beijunyi/leetcode/ds/TreeNode.java
+++ src/com/beijunyi/leetcode/ds/TreeNode.java
@@ -1,7 +1,6 @@
 package com.beijunyi.leetcode.ds;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class TreeNode {
   public int val;
@@ -15,16 +14,40 @@ public class TreeNode {
   public static TreeNode fromArray(Integer... nums) {
     if(nums == null || nums.length == 0 || nums[0] == null) return null;
     TreeNode root = new TreeNode(nums[0]);
-    Queue<TreeNode> nodes = new LinkedList<>();
-    nodes.offer(root);
+    Queue<TreeNode> q = new LinkedList<>();
+    q.offer(root);
     for(int i = 1; i < nums.length; i++) {
       Integer num = nums[i];
-      TreeNode head = i % 2 == 1 ? nodes.peek() : nodes.poll();
+      TreeNode head = i % 2 == 1 ? q.peek() : q.poll();
       TreeNode tail = num != null ? new TreeNode(num) : null;
       if(i % 2 == 1) head.left = tail;
       else head.right = tail;
-      if(tail != null) nodes.offer(tail);
+      if(tail != null) q.offer(tail);
     }
     return root;
   }
+
+  public static Integer[] toArray(TreeNode root) {
+    List<Integer> ret = new ArrayList<>();
+    if(root != null) {
+      Queue<TreeNode> q = new LinkedList<>();
+      int nullCount = 0;
+      q.offer(root);
+      ret.add(root.val);
+      while(!q.isEmpty()) {
+        TreeNode next = q.poll();
+        for(TreeNode child : Arrays.asList(next.left, next.right)) {
+          if(child != null) {
+            for(; nullCount > 0; nullCount--) ret.add(null);
+            q.offer(child);
+            ret.add(child.val);
+          } else {
+            nullCount++;
+          }
+        }
+      }
+    }
+    return ret.toArray(new Integer[ret.size()]);
+  }
+
 }

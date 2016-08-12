aeabc277f321e69904e6428da3e4a057cce8000b
junyi.bei <junyi.bei@accenture.com>
2016-08-12 18:09:11 +0100

    _245_ShortestWordDistanceThree set up

---
+++ src/com/beijunyi/leetcode/_245_ShortestWordDistanceThree.java
@@ -0,0 +1,40 @@
+package com.beijunyi.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beijunyi.leetcode.category.PremiumQuestion;
+import com.beijunyi.leetcode.category.difficulty.Medium;
+
+/**
+ * This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
+ *
+ * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+ *
+ * word1 and word2 may be the same and they represent two individual words in the list.
+ *
+ * For example,
+ * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ *
+ * Given word1 = “makes”, word2 = “coding”, return 1.
+ * Given word1 = "makes", word2 = "makes", return 3.
+ *
+ * Note:
+ * You may assume word1 and word2 are both in the list.
+ */
+public class _245_ShortestWordDistanceThree implements Medium, PremiumQuestion {
+
+  public interface Solution {
+    int shortestWordDistance(String[] words, String word1, String word2);
+  }
+
+  public static class Solution1 implements Solution {
+
+    @Override
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+      return 0;
+    }
+
+  }
+
+}

ed184cc0208cefde91bfe69861ad3d6ad260f4bd
beijunyi <beijunyi@gmail.com>
2016-08-13 16:18:34 +0100

    _245_ShortestWordDistanceThree

--- src/com/beijunyi/leetcode/_245_ShortestWordDistanceThree.java
+++ src/com/beijunyi/leetcode/_245_ShortestWordDistanceThree.java
@@ -1,6 +1,7 @@
 package com.beijunyi.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.beijunyi.leetcode.category.PremiumQuestion;
@@ -32,7 +33,43 @@ public class _245_ShortestWordDistanceThree implements Medium, PremiumQuestion {
 
     @Override
     public int shortestWordDistance(String[] words, String word1, String word2) {
-      return 0;
+      List<Integer> indices1 = new ArrayList<>();
+      List<Integer> indices2 = new ArrayList<>();
+      for(int i = 0; i < words.length; i++) {
+        if(words[i].equals(word1)) indices1.add(i);
+        if(words[i].equals(word2)) indices2.add(i);
+      }
+      int min = Integer.MAX_VALUE;
+      int i = 0, j = 0;
+      while(i < indices1.size() && j < indices2.size()) {
+        int diff = Math.abs(indices2.get(j) - indices1.get(i));
+        if(diff != 0) min = Math.min(min, diff);
+        if(i + 1 == indices1.size() || indices2.get(j) < indices1.get(i)) j++;
+        else i++;
+      }
+      return min;
+    }
+
+  }
+
+  public static void main(String args[]) {
+    String[] words;
+    String word1;
+    String word2;
+    int result;
+
+    for(Solution s : Arrays.asList(new Solution1())) {
+      words = new String[] {"practice", "makes", "perfect", "coding", "makes"};
+      word1 = "coding";
+      word2 = "practice";
+      result = s.shortestWordDistance(words, word1, word2);
+      System.out.println(result);
+
+      words = new String[] {"practice", "makes", "perfect", "coding", "makes"};
+      word1 = "makes";
+      word2 = "makes";
+      result = s.shortestWordDistance(words, word1, word2);
+      System.out.println(result);
     }
 
   }

4ae3a0420068816382b67fd45570ac82ebcbb707
beijunyi <beijunyi@gmail.com>
2016-08-17 01:12:53 +0100

    _269_AlienDictionary

--- src/com/beijunyi/leetcode/_269_AlienDictionary.java
+++ src/com/beijunyi/leetcode/_269_AlienDictionary.java
@@ -1,7 +1,11 @@
 package com.beijunyi.leetcode;
 
+import java.util.*;
+
 import com.beijunyi.leetcode.category.PremiumQuestion;
 import com.beijunyi.leetcode.category.difficulty.Hard;
+import com.beijunyi.leetcode.category.solution.KahnsAlgorithm;
+import com.beijunyi.leetcode.category.solution.TopologicalSorting;
 
 /**
  * There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You
@@ -30,12 +34,92 @@ public class _269_AlienDictionary implements Hard, PremiumQuestion {
     String alienOrder(String[] words);
   }
 
-  public static class Solution1 implements Solution {
+  public static class Solution1 implements Solution, KahnsAlgorithm, TopologicalSorting {
 
     @Override
     public String alienOrder(String[] words) {
-      return null;
+      if(words.length == 0) return "";
+      if(words.length == 1) return words[0];
+      boolean[][] matrix = new boolean[26][26];
+      boolean[] present = new boolean[26];
+      for(int i = 1; i < words.length; i++) {
+        if(!processWord(words[i - 1], words[i], matrix, present)) return "";
+      }
+      return findOrder(matrix, present);
+    }
+
+    private static boolean processWord(String prev, String curr, boolean[][] matrix, boolean[] present) {
+      int length = Math.min(prev.length(), curr.length());
+      int compared;
+      for(compared = 0; compared < length; compared++) {
+        int prevIdx = prev.charAt(compared)  - 'a';
+        int currIdx = curr.charAt(compared)  - 'a';
+        present[prevIdx] = true;
+        present[currIdx] = true;
+        if(prev.charAt(compared) != curr.charAt(compared)) {
+          if(matrix[currIdx][prevIdx]) return false;
+          matrix[prevIdx][currIdx] = true;
+          break;
+        }
+      }
+      for(int i = compared; i < prev.length(); i++) present[prev.charAt(i) - 'a'] = true;
+      for(int i = compared; i < curr.length(); i++) present[curr.charAt(i) - 'a'] = true;
+      return compared < length
+               || curr.length() <= prev.length(); // in the case that the compared parts of both are the same, check if the current word is the longer one
+    }
+
+    private static String findOrder(boolean[][] matrix, boolean[] present) {
+      char[] ret = new char[26];
+      int length = 0;
+      Queue<Integer> q = new LinkedList<>(findNodesWithNoIncomingEdge(matrix));
+      while(!q.isEmpty()) {
+        int from = q.poll();
+        if(!present[from]) continue;
+        ret[length++] = (char) ('a' + from);
+        for(int to = 0; to < 26; to++) {
+          if(matrix[from][to]) {
+            matrix[from][to] = false;
+            if(!hasIncomingEdge(to, matrix)) q.offer(to);
+          }
+        }
+      }
+      for(int i = 0; i < 26; i++) {
+        if(hasIncomingEdge(i, matrix)) return ""; // if there is any edge remained, there must be a cycle
+      }
+      return new String(ret, 0, length);
+    }
+
+    private static boolean hasIncomingEdge(int i, boolean matrix[][]) {
+      for(boolean[] to : matrix) {
+        if(to[i]) return true;
+      }
+      return false;
+    }
+
+    private static Set<Integer> findNodesWithNoIncomingEdge(boolean matrix[][]) {
+      Set<Integer> ret = new HashSet<>();
+      for(int i = 0; i < 26; i++) {
+        if(!hasIncomingEdge(i, matrix)) ret.add(i);
+      }
+      return ret;
     }
 
   }
+
+  public static void main(String args[]) {
+    String[] words;
+    String order;
+    for(Solution s : Arrays.asList(new Solution1())) {
+      words = new String[] {
+        "wrt",
+        "wrf",
+        "er",
+        "ett",
+        "rftt"
+      };
+      order = s.alienOrder(words);
+      System.out.println(order);
+    }
+  }
+
 }

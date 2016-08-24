bd9941fb76539f29e1d559d2f52b60391e7ce430
beijunyi <beijunyi@gmail.com>
2016-08-24 02:10:25 +0100

    _296_BestMeetingPoint

--- src/com/beijunyi/leetcode/_296_BestMeetingPoint.java
+++ src/com/beijunyi/leetcode/_296_BestMeetingPoint.java
@@ -89,11 +89,44 @@ public class _296_BestMeetingPoint implements Hard, PremiumQuestion {
     }
   }
 
+  /**
+   * This problem is converted to find the median value on x-axis and y-axis.
+   */
+  public static class Solution3 implements Solution {
+    @Override
+    public int minTotalDistance(int[][] grid) {
+      int m = grid.length;
+      int n = grid[0].length;
+
+      ArrayList<Integer> cols = new ArrayList<>();
+      ArrayList<Integer> rows = new ArrayList<>();
+      for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+          if(grid[i][j] == 1) {
+            cols.add(j);
+            rows.add(i);
+          }
+        }
+      }
+
+      int sum = 0;
+      for(Integer i : rows) {
+        sum += Math.abs(i - rows.get(rows.size() / 2));
+      }
+      Collections.sort(cols);
+      for(Integer i : cols) {
+        sum += Math.abs(i - cols.get(cols.size() / 2));
+      }
+
+      return sum;
+    }
+  }
+
   public static void main(String args[]) {
     int[][] grid;
     int result;
 
-    for(Solution s : Arrays.asList(new Solution1())) {
+    for(Solution s : Arrays.asList(new Solution1(), new Solution2(), new Solution3())) {
       grid = new int[][]{
         {1, 0, 0, 0, 1},
         {0, 0, 0, 0, 0},

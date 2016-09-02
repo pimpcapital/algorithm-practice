caa34761e47f9ee9abd7d2adca2606764444d3c5
junyi.bei <junyi.bei@accenture.com>
2016-09-02 11:18:35 +0100

    comment change

--- src/com/beijunyi/leetcode/_039_CombinationSum.java
+++ src/com/beijunyi/leetcode/_039_CombinationSum.java
@@ -7,18 +7,20 @@ import java.util.List;
 import com.beijunyi.leetcode.category.difficulty.Medium;
 
 /**
- * Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+ * Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate
+ * numbers sums to T.
  *
  * The same repeated number may be chosen from C unlimited number of times.
  *
  * Note:
- * All numbers (including target) will be positive integers.
- * Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
- * The solution set must not contain duplicate combinations.
+ *   All numbers (including target) will be positive integers.
+ *   Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+ *   The solution set must not contain duplicate combinations.
+ *
  * For example, given candidate set 2,3,6,7 and target 7,
- * A solution set is:
- * [7]
- * [2, 2, 3]
+ *   A solution set is:
+ *   [7]
+ *   [2, 2, 3]
  */
 public class _039_CombinationSum implements Medium {
 

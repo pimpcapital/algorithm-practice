100c7e38799f9a9f522c1a291bc1fc48d735f42f
beijunyi <beijunyi@gmail.com>
2016-09-13 22:42:25 +0100

    _374_GuessNumberHigherOrLower

--- src/com/beijunyi/leetcode/_374_GuessNumberHigherOrLower.java
+++ src/com/beijunyi/leetcode/_374_GuessNumberHigherOrLower.java
@@ -1,21 +1,19 @@
 package com.beijunyi.leetcode;
 
+import java.util.Arrays;
+
 import com.beijunyi.leetcode.category.difficulty.Easy;
 
 /**
  * We are playing the Guess Game. The game is as follows:
- *
  * I pick a number from 1 to n. You have to guess which number I picked.
- *
  * Every time you guess wrong, I'll tell you whether the number is higher or lower.
- *
  * You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
- *
- * -1 : My number is lower
- * 1 : My number is higher
- * 0 : Congrats! You got it!
+ *   -1 : My number is lower
+ *   1 : My number is higher
+ *   0 : Congrats! You got it!
  * Example:
- * n = 10, I pick 6.
+ *   n = 10, I pick 6.
  *
  * Return 6.
  */
@@ -27,7 +25,6 @@ public class _374_GuessNumberHigherOrLower implements Easy {
 
     int guessNumber(int n);
 
-    int guess(int num);
   }
 
   private static abstract class AbstractSolution implements Solution {
@@ -39,9 +36,39 @@ public class _374_GuessNumberHigherOrLower implements Easy {
       this.truth = truth;
     }
 
-    @Override
     public int guess(int num) {
-      return Integer.compare(num, truth);
+      return Integer.compare(truth, num);
+    }
+
+  }
+
+  public static class Solution1 extends AbstractSolution {
+
+    @Override
+    public int guessNumber(int n) {
+      int left = 1;
+      int right = n;
+      while(left < right) {
+        int mid = left + (right - left) / 2;
+        int result = guess(mid);
+        if(result == 0) return mid;
+        if(result == 1) left = mid + 1;
+        else right = mid - 1;
+      }
+      return left;
+    }
+
+  }
+
+  public static void main(String arg[]) {
+    int truth;
+    int max;
+
+    for(Solution s : Arrays.asList(new Solution1())) {
+      truth = 6;
+      max = 10;
+      s.init(truth);
+      System.out.println(s.guessNumber(max));
     }
   }
 

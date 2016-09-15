d956673f54a369cb705de9c13213b6b2246e3521
beijunyi <beijunyi@gmail.com>
2016-09-16 00:38:26 +0100

    _222_CountCompleteTreeNodes

---
+++ src/com/beijunyi/leetcode/_222_CountCompleteTreeNodes.java
@@ -0,0 +1,30 @@
+package com.beijunyi.leetcode;
+
+import com.beijunyi.leetcode.category.difficulty.Medium;
+import com.beijunyi.leetcode.ds.TreeNode;
+
+/**
+ * Given a complete binary tree, count the number of nodes.
+ *
+ * Definition of a complete binary tree from Wikipedia:
+ *   In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last
+ *   level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+ */
+public class _222_CountCompleteTreeNodes implements Medium {
+
+  public interface Solution {
+    int countNodes(TreeNode root);
+  }
+
+  public static class Solution1 implements Solution {
+    @Override
+    public int countNodes(TreeNode root) {
+      return 0;
+    }
+  }
+
+  public static void main(String args[]) {
+
+  }
+
+}

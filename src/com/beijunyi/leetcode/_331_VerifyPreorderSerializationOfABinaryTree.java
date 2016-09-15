396f329cc474f7d2bdcf9635d5339c0842d6fca6
beijunyi <beijunyi@gmail.com>
2016-09-15 23:52:24 +0100

    _331_VerifyPreorderSerializationOfABinaryTree

--- src/com/beijunyi/leetcode/_331_VerifyPreorderSerializationOfABinaryTree.java
+++ src/com/beijunyi/leetcode/_331_VerifyPreorderSerializationOfABinaryTree.java
@@ -43,10 +43,56 @@ public class _331_VerifyPreorderSerializationOfABinaryTree implements Medium {
     boolean isValidSerialization(String preorder);
   }
 
+  /**
+   * 1. values of numbers don't matter. there are only 2 types of nodes here: null vs non-null
+   * 2. starting with 1 slot
+   * 3. when you add a non-null node, you fill one existing slot and create 2 new slots.
+   * 4. when you add a null node, you fill one slot and add no new slots.
+   * 5. keep counting the total number of slots
+   * 6. when slot == 0, it either ends with success (no more node to process), or with failure (there are more nodes)
+   * 7. when all nodes are processed, check if all slots are filled
+   */
   public static class Solution1 implements Solution {
     @Override
     public boolean isValidSerialization(String preorder) {
-      return false;
+      String[] tokens = preorder.split(",");
+      int nulls = 1;
+      for(int i = 0; i < tokens.length; i++) {
+        String token = tokens[i];
+        switch(token) {
+          case "#":
+            if(--nulls == 0) return i == tokens.length - 1;
+            break;
+          default:
+            nulls++;
+        }
+      }
+      return nulls == 0;
+    }
+  }
+
+  /**
+   * Some used stack. Some used the depth of a stack. Here I use a different perspective. In a binary tree, if we
+   * consider null as leaves, then
+   *
+   * all non-null node provides 2 outdegree and 1 indegree (2 children and 1 parent), except root
+   * all null node provides 0 outdegree and 1 indegree (0 child and 1 parent).
+   * Suppose we try to build this tree. During building, we record the difference between out degree and in
+   *   degreediff = outdegree - indegree.
+   * When the next node comes, we then decrease diff by 1, because the node provides an in degree. If the node is not
+   * null, we increase diff by 2, because it provides two out degrees. If a serialization is correct, diff should never
+   * be negative and diff will be zero when finished.
+   */
+  public static class Solution2 implements Solution {
+    @Override
+    public boolean isValidSerialization(String preorder) {
+      String[] nodes = preorder.split(",");
+      int diff = 1;
+      for (String node: nodes) {
+        if (--diff < 0) return false;
+        if (!node.equals("#")) diff += 2;
+      }
+      return diff == 0;
     }
   }
 
@@ -55,6 +101,10 @@ public class _331_VerifyPreorderSerializationOfABinaryTree implements Medium {
     boolean result;
 
     for(Solution s : Arrays.asList(new Solution1())) {
+      preorder = "#,#";
+      result = s.isValidSerialization(preorder);
+      System.out.println(result);
+
       preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#";
       result = s.isValidSerialization(preorder);
       System.out.println(result);

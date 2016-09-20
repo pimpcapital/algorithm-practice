816ffa4f5eb3284bde9c373afbb96e2a2e2a738b
junyi.bei <junyi.bei@accenture.com>
2016-09-20 13:46:17 +0100

    _145_BinaryTreePostorderTraversal

--- src/com/beijunyi/leetcode/_145_BinaryTreePostorderTraversal.java
+++ src/com/beijunyi/leetcode/_145_BinaryTreePostorderTraversal.java
@@ -1,10 +1,9 @@
 package com.beijunyi.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import com.beijunyi.leetcode.category.difficulty.Hard;
+import com.beijunyi.leetcode.category.solution.Iterative;
 import com.beijunyi.leetcode.category.solution.Recursive;
 import com.beijunyi.leetcode.ds.TreeNode;
 
@@ -46,11 +45,105 @@ public class _145_BinaryTreePostorderTraversal implements Hard {
 
   }
 
+  public static class Solution2 implements Solution, Iterative {
+    @Override
+    public List<Integer> postorderTraversal(TreeNode root) {
+      List<Integer> ret = new ArrayList<>();
+      if(root == null) return ret;
+
+      Stack<State> stack = new Stack<>();
+      stack.push(new State(root, false, false));
+
+      while(!stack.isEmpty()) {
+        State state = stack.pop();
+        TreeNode node = state.node;
+        if(node == null) continue;
+        if(state.completedRight) {
+          ret.add(node.val);
+        } else if(state.completedLeft) {
+          stack.push(new State(node, true, true));
+          stack.push(new State(node.right, false, false));
+        } else {
+          stack.push(new State(node, true, false));
+          stack.push(new State(node.left, false, false));
+        }
+      }
+
+      return ret;
+    }
+
+    private static class State {
+      final TreeNode node;
+      final boolean completedLeft;
+      final boolean completedRight;
+
+      State(TreeNode node, boolean completedLeft, boolean completedRight) {
+        this.node = node;
+        this.completedLeft = completedLeft;
+        this.completedRight = completedRight;
+      }
+    }
+  }
+
+  public static class Solution3 implements Solution {
+    @Override
+    public List<Integer> postorderTraversal(TreeNode root) {
+      List<Integer> ret = new ArrayList<>();
+
+      Stack<TreeNode> stack = new Stack<>();
+      Stack<TreeNode> rights = new Stack<>();
+      pushAllLefts(root, stack);
+
+      while(!stack.isEmpty()) {
+        TreeNode next = stack.peek();
+        if(!rights.isEmpty() && rights.peek() == next) {
+          stack.pop();
+          rights.pop();
+          ret.add(next.val);
+        } else {
+          rights.push(next);
+          pushAllLefts(next.right, stack);
+        }
+      }
+
+      return ret;
+    }
+
+    private static void pushAllLefts(TreeNode node, Stack<TreeNode> stack) {
+      while(node != null) {
+        stack.push(node);
+        node = node.left;
+      }
+    }
+  }
+
+  /**
+   * Basic idea is using preorder traversal but traverse right child node first, then left child node.
+   * Right->Left->Curr, then reverse the result
+   */
+  public static class Solution4 implements Solution {
+    @Override
+    public List<Integer> postorderTraversal(TreeNode root) {
+      LinkedList<Integer> ret = new LinkedList<>();
+      Stack<TreeNode> stack = new Stack<>();
+      if(root == null) return ret;
+
+      stack.push(root);
+      while(!stack.isEmpty()) {
+        TreeNode cur = stack.pop();
+        ret.addFirst(cur.val);
+        if(cur.left != null) stack.push(cur.left);
+        if(cur.right != null) stack.push(cur.right);
+      }
+      return ret;
+    }
+  }
+
   public static void main(String args[]) {
     TreeNode root;
     List<Integer> result;
 
-    for(Solution s : Arrays.asList(new Solution1())) {
+    for(Solution s : Arrays.asList(new Solution1(), new Solution2(), new Solution3(), new Solution4())) {
       root = TreeNode.fromArray(1, null, 2, 3);
       result = s.postorderTraversal(root);
       System.out.println(result);
